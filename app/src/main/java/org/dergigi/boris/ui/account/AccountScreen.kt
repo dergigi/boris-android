e73cfafcebb497c86cc19bceffcfa2cef586406e
Gigi <dergigi@pm.me>
2026-08-16 23:31:38 +0200

    chore: drop unused IconButton import from AccountScreen

--- app/src/main/java/org/dergigi/boris/ui/account/AccountScreen.kt
+++ app/src/main/java/org/dergigi/boris/ui/account/AccountScreen.kt
@@ -20,7 +20,6 @@ import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
