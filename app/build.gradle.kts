45202b305d20f7fa7cbd57dfd061b891ea5693b9
Gigi <dergigi@pm.me>
2026-08-23 22:56:11 +0100

    chore: release 1.5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "org.dergigi.boris"
         minSdk = 26
         targetSdk = 35
-        versionCode = 164
-        versionName = "1.5.3"
+        versionCode = 165
+        versionName = "1.5.4"
         buildConfigField("String", "GIT_COMMIT", "\"${gitCommit()}\"")
     }
 
