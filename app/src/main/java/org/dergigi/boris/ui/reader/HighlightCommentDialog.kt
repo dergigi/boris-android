124ae36ba7b911db271e118d5bf9e10bc293556d
Gigi <dergigi@pm.me>
2026-09-05 21:28:48 +0100

    fix(reader): import dp in the highlight comment dialog

--- app/src/main/java/org/dergigi/boris/ui/reader/HighlightCommentDialog.kt
+++ app/src/main/java/org/dergigi/boris/ui/reader/HighlightCommentDialog.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import org.dergigi.boris.R
 
 data class PendingHighlightAnnotation(
